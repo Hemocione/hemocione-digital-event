f8c155bc730bd12237926ccace7ecf7c24af883f
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2023-09-09 21:48:50 -0300

    use page fallback

---
+++ pages/[...slug].vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page index-page">
+    <img src="/images/logo-vertical-branca.svg" class="logo" alt="Hemocione Logo"/>
+  </div>
+</template>
+
+<style scoped>
+.index-page {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.logo {
+  height: 60%;
+  max-width: 80%;
+}
+</style>
