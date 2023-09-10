5ff59237c18d0dc097d2e3749c8a06829e8b1379
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2023-09-09 21:19:30 -0300

    add some SEO stuff!

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Hemocione Eventos',
+  ogTitle: 'Hemocione Eventos',
+  description: 'Hemocione Eventos é um aplicativo de gerenciamento de eventos de doação de sangue. Utilize-o para gerenciar as filas de doação de sangue de seus eventos e muito mais!',
+  ogDescription: 'Hemocione Eventos é um aplicativo de gerenciamento de eventos de doação de sangue. Utilize-o para gerenciar as filas de doação de sangue de seus eventos e muito mais!',
+  ogImage: 'https://cdn.discordapp.com/attachments/757012940804194367/1150223307472384122/Horizontal-Cor-FP.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
