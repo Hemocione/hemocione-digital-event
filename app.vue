1f1e1fb78e0d0be7bfbf71af04d4dba1ffd96709
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2023-10-07 10:39:05 -0300

    block mobile zoom

--- app.vue
+++ app.vue
@@ -4,14 +4,19 @@
 
 <script setup lang="ts">
 useHead({
-  title: 'Hemocione Eventos',
-})
+  title: "Hemocione Eventos",
+});
 useServerSeoMeta({
-  title: 'Hemocione Eventos',
-  ogTitle: 'Hemocione Eventos',
-  description: 'Hemocione Eventos é um aplicativo de gerenciamento de eventos de doação de sangue. Utilize-o para gerenciar as filas de doação de sangue de seus eventos e muito mais!',
-  ogDescription: 'Hemocione Eventos é um aplicativo de gerenciamento de eventos de doação de sangue. Utilize-o para gerenciar as filas de doação de sangue de seus eventos e muito mais!',
-  ogImage: 'https://cdn.discordapp.com/attachments/757012940804194367/1150223307472384122/Horizontal-Cor-FP.png',
-  twitterCard: 'summary_large_image',
-})
+  title: "Hemocione Eventos",
+  ogTitle: "Hemocione Eventos",
+  description:
+    "Hemocione Eventos é um aplicativo de gerenciamento de eventos de doação de sangue. Utilize-o para gerenciar as filas de doação de sangue de seus eventos e muito mais!",
+  ogDescription:
+    "Hemocione Eventos é um aplicativo de gerenciamento de eventos de doação de sangue. Utilize-o para gerenciar as filas de doação de sangue de seus eventos e muito mais!",
+  ogImage:
+    "https://cdn.discordapp.com/attachments/757012940804194367/1150223307472384122/Horizontal-Cor-FP.png",
+  twitterCard: "summary_large_image",
+  viewport:
+    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+});
 </script>
