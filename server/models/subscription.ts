868a945975e30603f28c4d325f7aea1c8cd6daa4
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2024-02-12 14:44:34 -0300

    add subscription model

---
+++ server/models/subscription.ts
@@ -0,0 +1,64 @@
+import type { InferSchemaType } from "mongoose";
+import { Schema, Types, model } from "mongoose";
+
+const SubscriptionSchema = new Schema(
+  {
+    eventSlug: {
+      type: String,
+      required: true,
+    },
+    hemocioneId: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    name: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    email: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    phone: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    code: {
+      type: String,
+      required: true,
+      default: () => getRandomString(6),
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+    schedule: {
+      _id: {
+        type: Types.ObjectId,
+        required: true,
+      },
+      startAt: {
+        type: Date,
+        required: true,
+      },
+      endAt: {
+        type: Date,
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+SubscriptionSchema.index({ hemocioneId: 1, eventSlug: 1 }, { unique: true, partialFilterExpression: { deletedAt: null } });
+SubscriptionSchema.index({ code: 1, eventSlug: 1 }, { unique: true });
+
+export type SubscriptionSchema = InferSchemaType<typeof SubscriptionSchema>;
+
+export const Event = model<SubscriptionSchema>("Event", SubscriptionSchema);
