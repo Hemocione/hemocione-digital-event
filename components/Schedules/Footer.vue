e47320df2cbfdccdd669c0168cfb9b593b6978c4
Luiz E <doginuwu@gmail.com>
2024-02-12 15:48:22 -0300

    chore: add simulated loading schedule button

--- components/Schedules/Footer.vue
+++ components/Schedules/Footer.vue
@@ -1,12 +1,19 @@
 <template>
   <CommonCoolFooter height="fit-content" desktop-border-radius="0">
-    <ElButton type="primary" size="large" @click="goToSchedule">
+    <ElButton
+      type="primary"
+      size="large"
+      :loading="state.loading"
+      @click="selectSchedule"
+    >
       Confirmar
     </ElButton>
   </CommonCoolFooter>
 </template>
 
 <script setup lang="ts">
+import { reactive } from "vue";
+
 const props = defineProps({
   eventSlug: {
     type: String,
@@ -18,10 +25,36 @@ const props = defineProps({
   },
 });
 
-function goToSchedule() {
-  if (!props.selectedScheduleId) return;
+const state = reactive({
+  loading: false,
+});
 
-  navigateTo(`/event/${props.eventSlug}/ticket`);
+async function selectSchedule() {
+  if (!props.selectedScheduleId) {
+    ElNotification({
+      title: "Horário não selecionado",
+      message: "Selecione um horário para continuar.",
+      type: "error",
+    });
+
+    return;
+  }
+
+  state.loading = true;
+
+  try {
+    // TODO: Implement the API call to save the selected schedule
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    navigateTo(`/event/${props.eventSlug}/ticket`);
+  } catch (error) {
+    ElNotification({
+      title: "Erro ao selecionar o horário",
+      message: "Tente novamente mais tarde.",
+      type: "error",
+    });
+  }
+
+  state.loading = false;
 }
 </script>
 
