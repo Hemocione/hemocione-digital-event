785737a60a846bcc39bf0e2fc3778174a92561da
Luiz E <doginuwu@gmail.com>
2024-02-13 11:32:06 -0300

    chore: add fallback to copy event on desktop

--- components/Ticket/Footer.vue
+++ components/Ticket/Footer.vue
@@ -1,11 +1,6 @@
 <template>
   <CommonCoolFooter height="fit-content" desktop-border-radius="0">
-    <ElButton
-      v-if="hasShareAPI"
-      type="primary"
-      size="large"
-      @click="shareEvent"
-    >
+    <ElButton type="primary" size="large" @click="shareEvent">
       Compartilhar evento
     </ElButton>
   </CommonCoolFooter>
@@ -19,15 +14,25 @@ const props = defineProps({
   },
 });
 
-const hasShareAPI = !!navigator.share;
-
 function shareEvent() {
   const baseUrl = window.location.origin;
+  const url = `${baseUrl}/event/${props.eventSlug}`;
 
-  navigator.share({
-    title: "Hemocione",
-    text: "Estou participando de um evento Hemocione! Venha comigo!",
-    url: `${baseUrl}/event/${props.eventSlug}`,
+  if (navigator.share) {
+    navigator.share({
+      title: "Hemocione",
+      text: "Estou participando de um evento Hemocione! Venha comigo!",
+      url,
+    });
+
+    return;
+  }
+
+  navigator.clipboard.writeText(url);
+  ElNotification({
+    title: "Link copiado",
+    message: "O link do evento foi copiado para a área de transferência.",
+    type: "success",
   });
 }
 </script>
