57d624c3d8bdf84e403e496a3698e7284b8c8170
hannahbgolds <hannahbgoldstein@gmail.com>
2024-08-01 21:58:22 -0300

    deletedAt route

--- server/api/v1/event/[eventSlug]/external-volunteer/index.post.ts
+++ server/api/v1/event/[eventSlug]/external-volunteer/index.post.ts
@@ -8,13 +8,13 @@ export default defineEventHandler(async (event) => {
 
   const hemoEvent = await getEventBySlug(eventSlug);
 
-  if (!hemoEvent||!hemoEvent.externalVolunteers?.enabled) {
+  if (!hemoEvent || !hemoEvent.externalVolunteers?.enabled) {
     throw createError({ statusCode: 404, statusMessage: "Event not found" });
   }
   if (hemoEvent.externalVolunteers.occupiedSlots >= hemoEvent.externalVolunteers.slots){
     throw createError({ statusCode: 404, statusMessage: "All slots are occupied" });
   }
-  
+
   const externalVolunteer = await createExternalVolunteer(eventSlug, user);
 
   return externalVolunteer;
--- server/api/v1/event/[eventSlug]/external-volunteer/mine.delete.ts
+++ server/api/v1/event/[eventSlug]/external-volunteer/mine.delete.ts
@@ -1,10 +1,15 @@
 import { useHemocioneUserAuth } from "~/server/services/auth";
+import { getEventBySlug } from "~/server/services/event";
 import { deleteExternalVolunteer } from "~/server/services/externalVolunteer";
 
 export default defineEventHandler(async (event) => {
   const user = useHemocioneUserAuth(event);
   const eventSlug = String(getRouterParam(event, "eventSlug"));
+  const hemoEvent = await getEventBySlug(eventSlug);
 
-  const externalVolunteer = await deleteExternalVolunteer(eventSlug, user.id);
-  return externalVolunteer;
+  if (!hemoEvent || !hemoEvent.externalVolunteers?.enabled) {
+    throw createError({ statusCode: 404, statusMessage: "Event not found" });
+  }
+
+  await deleteExternalVolunteer(eventSlug, user.id);
 });
