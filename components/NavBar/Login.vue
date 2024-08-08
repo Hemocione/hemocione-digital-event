b6abb42a67eca1da05706c93b7a6a093f39f20c7
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2024-08-07 21:47:23 -0300

    use confirmation modal

--- components/NavBar/Login.vue
+++ components/NavBar/Login.vue
@@ -3,11 +3,34 @@
     v-if="loggedIn"
     :button-text="buttonText"
     :logged-in="loggedIn"
-    @click="signOut"
+    @click="requestSignOut"
   />
   <NuxtLink v-else :to="authUrl" class="auth-button" external>
     <NavBarAuthButton :button-text="buttonText" :logged-in="loggedIn" />
   </NuxtLink>
+  <ElDialog
+    v-model="dialogVisible"
+    :title="buttonText"
+    width="300px"
+    align-center
+  >
+    <p>Tem certeza que deseja sair?</p>
+    <template #footer>
+      <ElButton
+        size="large"
+        @click="dialogVisible = false"
+        style="width: calc(50% - 0.25rem); margin-right: 0.5rem"
+        >Cancelar</ElButton
+      >
+      <ElButton
+        size="large"
+        type="primary"
+        @click="signOut"
+        style="width: calc(50% - 0.25rem)"
+        >Sair</ElButton
+      >
+    </template>
+  </ElDialog>
 </template>
 <script setup lang="ts">
 const userStore = useUserStore();
@@ -16,6 +39,7 @@ const user = computed(() => userStore.user);
 const loggedIn = computed(() => userStore.loggedIn);
 const config = useRuntimeConfig();
 const { hemocioneIdUrl, siteUrl } = config.public;
+const dialogVisible = ref(false);
 
 const authUrl = computed(() => {
   const currentPath = route.fullPath;
@@ -25,8 +49,13 @@ const authUrl = computed(() => {
   return `${hemocioneIdUrl}?redirect=${encodedRedirectUrl}`;
 });
 
+const requestSignOut = () => {
+  dialogVisible.value = true;
+};
+
 const signOut = () => {
   userStore.signOut();
+  dialogVisible.value = false;
 };
 
 const buttonText = computed(() => {
