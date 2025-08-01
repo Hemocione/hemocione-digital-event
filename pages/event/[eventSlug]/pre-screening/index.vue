f533aad83c6a5cc53f00eb508f2011689ab406b0
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2025-08-01 14:30:33 -0300

    hotfix pre-screening page min height for app

--- pages/event/[eventSlug]/pre-screening/index.vue
+++ pages/event/[eventSlug]/pre-screening/index.vue
@@ -4,22 +4,26 @@
       <CommonEventHeader :event-name="eventConfig.name" @back="goBack" />
       <main class="content">
         <p class="description">
-          Para continuar a inscrição, responda o questionário e verifique se você pode realizar a doação.
+          Para continuar a inscrição, responda o questionário e verifique se
+          você pode realizar a doação.
         </p>
-  
+
         <p class="note">
-          Este questionário serve como uma orientação inicial com perguntas frequentes sobre a doação,
-          mas não substitui a triagem realizada por profissionais de saúde no dia e no local da doação.
+          Este questionário serve como uma orientação inicial com perguntas
+          frequentes sobre a doação, mas não substitui a triagem realizada por
+          profissionais de saúde no dia e no local da doação.
         </p>
-  
-        <NuxtImg src="images/donation-illustration.svg" alt="Ilustração de Doação" class="illustration" />
+
+        <NuxtImg
+          src="images/donation-illustration.svg"
+          alt="Ilustração de Doação"
+          class="illustration"
+        />
       </main>
-      <PreScreeningMiddleFooter 
-      :event-slug="eventSlug" />
+      <PreScreeningMiddleFooter :event-slug="eventSlug" />
     </div>
   </div>
 </template>
-
 
 <script setup lang="ts">
 import { useRouter } from "vue-router";
@@ -27,7 +31,7 @@ import { useRoute } from "vue-router";
 import { useEventStore } from "~/stores/event";
 
 definePageMeta({
-middleware: ["auth"],
+  middleware: ["auth"],
 });
 
 const route = useRoute();
@@ -39,22 +43,22 @@ const eventSlug = route.params.eventSlug as string;
 const eventConfig = await eventStore.getEvent(eventSlug);
 
 function goBack() {
-navigateTo(`/event/${eventSlug}`);
+  navigateTo(`/event/${eventSlug}`);
 }
 </script>
 
 <style scoped>
 .pre-screening-middle-page {
   width: 100%;
-max-width: var(--hemo-page-max-width);
-margin: 0 auto;
-min-height: 93dvh;
-display: flex;
-flex-direction: column;
-align-items: flex-start;
-justify-content: flex-start;
-background-color: var(--hemo-color-white);
-color: var(--hemo-color-text-secondary);
+  max-width: var(--hemo-page-max-width);
+  margin: 0 auto;
+  min-height: calc(100dvh - var(--hemo-navbar-height));
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  background-color: var(--hemo-color-white);
+  color: var(--hemo-color-text-secondary);
 }
 
 .header {
