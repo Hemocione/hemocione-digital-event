b25ef8e6c19181cb5e84d8f1322678006c993410
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-09-24 09:43:16 -0300

    Add assert secret

--- server/api/v1/points/ondedoar/sync.get.ts
+++ server/api/v1/points/ondedoar/sync.get.ts
@@ -1,6 +1,9 @@
 import { getPointsOndeDoar } from "~/server/services/event";
+import { assertSecretAuth } from "../../../../services/auth";
 
 export default defineEventHandler(async (event) => {
+  assertSecretAuth(event);
+
   const { old } = getQuery(event);
   const shouldGetOldEvents = String(old) === "true";
   const events = await getPointsOndeDoar(shouldGetOldEvents);
