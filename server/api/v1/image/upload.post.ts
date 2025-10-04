8d2ca552e0872bd586c529285ec2cd35ad5678e2
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2025-10-03 22:43:46 -0300

    Add crypto package and use randomUUID for unique image upload paths

--- server/api/v1/image/upload.post.ts
+++ server/api/v1/image/upload.post.ts
@@ -1,5 +1,6 @@
 import { assertSecretAuth, useHemocioneUserAuth } from "~/server/services/auth";
 import { uploadImage } from "~/server/services/cdn";
+import { randomUUID } from "crypto";
 
 export default defineEventHandler(async (event) => {
   const headers = getRequestHeaders(event);
@@ -17,7 +18,8 @@ export default defineEventHandler(async (event) => {
     assertSecretAuth(event);
   }
 
-  const extraPrefix = user ? `users/${user.id}` : "private";
+  const uuid = randomUUID();
+  const extraPrefix = `${user ? "users" : "private"}/${uuid}`;
   const url = await uploadImage(event, extraPrefix);
   return {
     url,
